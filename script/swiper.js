68da212bb6333a432af88b9881400f4a0d40d2cf
Sergey Tablet <martsnov@hotmail.com>
2022-05-06 22:48:53 +0300

    done singl & error page

---
+++ script/swiper.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper('.swiper', {
+    // Navigation arrows
+
+    slidesPerView: 3,
+    spaceBetween: 40,
+    navigation: {
+      nextEl: '.arrow-slider-right',
+      prevEl: '.arrow-slider-left',
+    },
+  });
